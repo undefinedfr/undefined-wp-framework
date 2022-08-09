717cb7afc064288b74ed1fb696b3cf299dc3d490
Nicolas RIVIERE <hello@undefined.fr>
2022-08-09 11:09:26 +0200

    improvement for Gutenberg Blocks

--- src/Gutenberg/Block.php
+++ src/Gutenberg/Block.php
@@ -68,7 +68,6 @@ class Block
     public function __construct($stylesheet = null)
     {
         $this->title = __($this->title);
-        $this->render_template = !empty($this->render_template) ? $this->render_template : (get_template_directory() . '/templates/partial/block/' . $this->name . '.twig');
         add_action( 'acf/init', [$this, 'registerBlock'] );
         add_action( 'enqueue_block_editor_assets', [$this, 'loadAssets'] );
     }
@@ -78,25 +77,29 @@ class Block
      */
     public function registerBlock()
     {
-       if ( function_exists( 'acf_register_block' ) ) {
-           acf_register_block(array(
-               'name'            => $this->name,
-               'title'           => $this->title,
-               'description'     => $this->description,
-               'render_template' => $this->render_template,
-               'render_callback' => !empty($this->render_callback) ? $this->render_callback : [$this, 'render'],
-               'category'        => $this->category,
-               'icon'            => $this->icon,
-               'mode'            => $this->mode,
-               'keywords'        => array_merge([$this->name], $this->keywords)
-           ));
-       }
+        if ( function_exists( 'acf_register_block' ) ) {
+
+            $this->render_template = apply_filters('undfnd_gutenberg_bloc_template', (get_template_directory() . '/templates/partial/block/' . $this->name . '.twig'), $this->name);
+            $this->render_callback = apply_filters('undfnd_gutenberg_bloc_callback', [$this, 'render'], $this->name);
+
+            acf_register_block(array(
+                'name'            => $this->name,
+                'title'           => $this->title,
+                'description'     => $this->description,
+                'render_template' => $this->render_template,
+                'render_callback' => $this->render_callback,
+                'category'        => $this->category,
+                'icon'            => $this->icon,
+                'mode'            => $this->mode,
+                'keywords'        => array_merge([$this->name], $this->keywords)
+            ));
+        }
     }
 
     /**
      * Render block with twig
      */
-    public function render()
+    public function render( $block )
     {
         Timber::render( $this->render_template, ['block' => $block] );
     }
